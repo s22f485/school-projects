389412d7bc1c9b5c478ab6512b73ea051c94a3da
MacKenzie O'Bleness <mackenzie.oblenes@gmail.com>
2015-12-05 17:29:37 -0700

    minor tweaks for another 1.5%

--- CSCI-361FinalProject/sieve.asm
+++ CSCI-361FinalProject/sieve.asm
@@ -9,11 +9,19 @@ space:	.asciiz	" "		# whitespace to separate prime numbers
 main:	li	$s0, 0x00000000	# initialize $s0 with zeros
 	li	$s1, 0x11111111	# initialize $s1 with ones
 	li	$t9, 200	# find prime numbers from 2 to $t9
+	mtc1 $t9, $f1
+	cvt.d.w $f2, $f1
+	sqrt.d $f2, $f2
+	cvt.w.d $f1, $f2
+	mfc1 $t8, $f1
+	
+	li	$t6, 1
+	li	$t7, 2
 
 	add	$s2, $sp, 0	# backup bottom of stack address in $s2
 
-	li	$v0, 1		# system code to print integer
-	add	$a0, $s0, 2	# the argument will be our prime number in $t3
+	li 	$v0, 1		# system code to print integer
+	add	$a0, $t7, 0	# the argument will be our prime number in $t3
 	syscall			# print it!
 	li	$v0, 4		# system code to print string
 	la	$a0, space	# the argument will be a whitespace
@@ -22,32 +30,31 @@ main:	li	$s0, 0x00000000	# initialize $s0 with zeros
 	li $t0, 3
 	
 init:	sb $s1, 0($sp)
-	sub $sp, $sp, 1
-	add $t0, $t0, 2
+	sub $sp, $sp, $t6
+	add $t0, $t0, $t7
 	ble $t0, $t9, init
 	
 	li $t0, 1
-	add $s3, $s2, 1
+	add $s3, $s2, $t6
 	
-outer:	add $t0, $t0, 2
-	sub $s3, $s3, 1
-	mul $t1, $t0, $t0
-	bgt $t1, $t9, print
+outer:	add $t0, $t0, $t7
+	bgt $t0, $t8, print # if count is greater than the sqrt($t9), then print 
+	sub $s3, $s3, $t6
+	
 	
 	lb $t3, ($s3)
 	
 	beq $t3, $s0, outer
 	
 	# print prime
-	li	$v0, 1		# system code to print integer
+	li	$v0, 1 		# system code to print integer
 	add	$a0, $s0, $t0	# the argument will be our prime number in $t3
 	syscall			# print it!
 	li	$v0, 4		# system code to print string
 	la	$a0, space	# the argument will be a whitespace
 	syscall
 	
-	add $t1, $s3, 0 # og count address 
-	#sll $t4, $t0, 1 # mem jump amount
+	add $t1, $s3, 0 # og count address
 	add $t2, $t0, 0 # current count
 	sll $t3, $t0, 1 # count jump amount
 	
@@ -57,10 +64,10 @@ inner:	add $t2, $t2, $t3 	# increment count
 	sb	$s0, ($t1)
 	j	inner		# some multiples left? go back to inner loop
 
-count:	add	$t0, $t0, 2	# increment counter variable
-	sub 	$s3, $s3, 1
+count:	add	$t0, $t0, $t7	# increment counter variable
 print:	bgt	$t0, $t9, exit	# make sure to exit when all numbers are done
-
+	sub 	$s3, $s3, $t6
+	
 	lb	$t3, ($s3)	# load the content into $t3
 	beq	$t3, $s0, count	# only 0's? go back to count loop
 
